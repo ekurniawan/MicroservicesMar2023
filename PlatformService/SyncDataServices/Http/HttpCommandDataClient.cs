2fbfe2121e9fc01c054aca807d264e294255cfec
ekurniawan <erick.kurniawan@gmail.com>
2023-03-29 10:54:07 +0700

    menambahkan command services

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient,IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
